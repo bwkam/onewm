5c3146b6fe63ed91fcea1ed4edf1b4d509ea94c5
bwkam <userdev987@gmail.com>
2023-03-19 12:59:44 +0000

    bspwm done

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# Haxe project
+# OneWM
 
-This is an example Haxe project scaffolded by Visual Studio Code.
+## THIS IS STILL WIP
 
-Without further changes the structure is following:
+- A window manager agnostic framework for configuring UNIX window managers.
 
- * `src/Main.hx`: Entry point Haxe source file
- * `build.hxml`: Haxe command line file used to build the project
- * `README.md`: This file
+- All window managers serve the same purpose, but the problem is that, with the variety of all the window managers out there, it takes time some time to get used to another window manager's configuration language/syntax/options/API. Wouldn't it be nicer to have a single way of configuring bspwm, qtile, and awesomewm? All that by just simply telling your config to use which window manager you want and leave the rest of configuration as is.
+
+## WMs
+
+- [x] bspwm
+- [ ] awesomewm
+- [ ] i3
+- [ ] qtile
+- [ ] dk
+- [ ] leftwm
--- src/utils/Utils.hx
+++ src/utils/Utils.hx
@@ -1,5 +1,6 @@
 package utils;
 
+import haxe.Json;
 import haxe.macro.Expr;
 
 using Lambda;
@@ -13,8 +14,27 @@ class Utils {
 			haxe.macro.Context.error('Failed to load json: $e', haxe.macro.Context.currentPos());
 		}
 	}
-
-	macro public static function return_expr(expr:haxe.macro.Expr) {
-		return macro $expr;
-	}
+	// macro public static function return_expr(?expr:haxe.macro.Expr) {
+	// 	return build_json();
+	// }
+	// public static function build_json() {
+	// 	var block = [];
+	// 	var json = Utils.load("config.json");
+	// 	var data = new json2object.JsonParser<Models.JsonModel>().fromJson(Json.stringify(json), "config.json");
+	// 	// var config:Map<String, Models.ConfigModel> = data.config;
+	// 	// var workspaces:Array<String> = data.workspaces;
+	// 	var rules:Array<Map<String, Models.RuleModel>> = data.rules;
+	// 	// var exprs_arr = [];
+	// 	// build the rules
+	// 	for (rule in rules) { // rule is a map
+	// 		var expr = macro null;
+	// 		for (i in rule) {
+	// 			expr = macro Rule.add_rule($v{i});
+	// 			block.push(expr);
+	// 		}
+	// 	}
+	// 	// build the config
+	// 	// build_config_expr({border_width: "10", top_padding: "5"});
+	// 	return macro $b{block};
+	// }
 }
