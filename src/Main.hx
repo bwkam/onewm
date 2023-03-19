058646d3c96012fe0956f03e40f942b63be97f4e
bwkam <userdev987@gmail.com>
2023-03-19 11:52:42 +0000

    some basic things

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,4 @@
+import haxe.macro.Context;
 import haxe.Json;
 import sys.io.File;
 import utils.Utils;
@@ -8,7 +9,7 @@ using Lambda;
 @:build(wms.bspwm.macros.Macro.gen_config())
 class Main {
 	static function main() {
-		Utils.build_json();
+		Utils.return_expr(macro ${build_json()});
 		setup();
 		inject();
 	}
@@ -19,5 +20,34 @@ class Main {
 
 	public static function inject() {
 		File.append("config").writeString(extra() + '\n');
+	}
+
+	public static function build_json() {
+		var block = [];
+		var json = Utils.load("config.json");
+		var data = new json2object.JsonParser<Models.JsonModel>().fromJson(Json.stringify(json), "config.json");
+		// var config:Map<String, Models.ConfigModel> = data.config;
+		// var workspaces:Array<String> = data.workspaces;
+		var rules:Array<Map<String, Models.RuleModel>> = data.rules;
+		// var exprs_arr = [];
+
+		// build the rules
+		for (rule in rules) { // rule is a map
+			var expr = macro null;
+			for (i in rule) {
+				expr = macro Rule.add_rule($v{i});
+				block.push(expr);
+			}
+		}
+
+		// build the config
+		// build_config_expr({border_width: "10", top_padding: "5"});
+
+		return macro $b{block};
+	}
+
+	macro public static function build_config_expr(expr:haxe.macro.Expr) {
+		var types = Context.getModule("wms.bspwm.Bspwm");
+		return macro null;
 	}
 }
--- src/utils/Utils.hx
+++ src/utils/Utils.hx
@@ -1,6 +1,5 @@
 package utils;
 
-import haxe.macro.Context;
 import haxe.macro.Expr;
 
 using Lambda;
@@ -15,14 +14,7 @@ class Utils {
 		}
 	}
 
-	macro public static function build_json() {
-		var block = [];
-		var json = Utils.load("config.json");
-		var data = new json2object.JsonParser<Models.JsonModel>().fromJson(Json.stringify(json), "config.json");
-		var config:Map<String, Models.ConfigModel> = data.config;
-		var workspaces:Array<String> = data.workspaces;
-		var rules:Array<Map<String, Models.RuleModel>> = data.rules;
-
-		return macro $b{block};
+	macro public static function return_expr(expr:haxe.macro.Expr) {
+		return macro $expr;
 	}
 }
